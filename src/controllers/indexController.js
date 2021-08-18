59390716fac9cd5ba2d80e5049f780cd58e6a0a3
Joniruiz <ruiz.jonathan2018@gmail.com>
2021-08-17 21:31:56 -0300

    input search funcional de productos en header users

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,4 +1,4 @@
-let {getPc} = require('../data/dataBase')
+let {getPc, getProducts} = require('../data/dataBase')
 
 
 module.exports = {
@@ -13,5 +13,21 @@ module.exports = {
         res.render('about', {
             title:"about-us"
         })
-    }
+    },
+    search: (req, res) => {
+		let result = []
+		getProducts.forEach(product => {
+			if (product.modelo.toLowerCase().includes(req.query.keywords.toLowerCase())){
+				result.push(product)
+			}
+		});
+		
+		 	res.render('results', {
+            title: 'resultados de busqueda',
+			result,
+			search: req.query.keywords,
+			
+		}) 
+        /* res.send(result) */
+	}
 }

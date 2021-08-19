313cd9aa505d23a03c753793d6ae792eb1f83bb0
Joniruiz <ruiz.jonathan2018@gmail.com>
2021-08-19 14:59:46 -0300

    EL usuario ya se puede registrar desde la web

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,5 @@
+const { getUsers, addUsers } = require("../data/dataBase");
+
 module.exports = {
     index: (req,res) => {
         res.render('userProfile', {
@@ -8,6 +10,38 @@ module.exports = {
         res.render('register', {
             title: "Registro"
         })
+    },
+    userRegister:(req,res) => {
+        let lastId = 1;   // Creo una variable con un id , esta  variable representa un contador basicamente
+
+        getUsers.forEach(user => {   // recorro el array de productos buscando un producto
+            if(user.id > lastId){    // Si el id del producto es mayor que la variable
+                lastId = user.id     //va a valer 1
+            }
+        });
+
+
+        let nuevoUsuario = {
+            
+
+            id: lastId + 1,  // para crear un nuevo producto , le estamos diciendo que el id que recibe el nuevo producto , va a ser el valor del lastid, osea el ultimo producto que  itero +1
+            nombre: req.body.nombre.trim(),  // escribimos el modelo , con el parametro que recibimos por el  objeto body que nos envia la pagina y con el "." estamos entrando en el objeto
+            apellido: req.body.apellido.trim(),
+            telefono: req.body.telefono.trim(),
+            email: req.body.email.trim(),
+            password: req.body.password.trim(),
+            repeatPassword: req.body.repeatPassword.trim(),
+            admin: false,
+            image:'user.jpg'
+        }
+
+        
+
+        getUsers.push(nuevoUsuario);   // le estamos metiendo a la variable getproducts que es la que tiene todos los productos el nuevo producto que estamos creando
+        
+        addUsers(getUsers);  // esta funcion escribe el json  y recibe como parametro la base de datos donde va a ser escrito
+        
+        res.redirect('/')  // una vez agregado el producto nos redirecciona  a la vista de productos
     },
     finishBuy:(req,res) =>{
         res.render('finishBuying', {
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
-let {index , register, finishBuy }= require('../controllers/userController')
+let {index , register, finishBuy, userRegister }= require('../controllers/userController')
 
 /* GET home page. */
+
 router.get('/', index)
+
+
 router.get('/register', register)
+router.post('/register',userRegister )
+
 router.get('/shop', finishBuy)
 
 

613801b1d984412000e266e694db72c2e27520f1
Joniruiz <ruiz.jonathan2018@gmail.com>
2021-09-28 12:20:44 -0300

    funciona en el index

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -5,7 +5,10 @@ const db = require('../database/models')
 
 module.exports = {
     index: (req,res) => {
-        db.Product.findAll()
+        db.Product.findAll({
+            include:[{association:'images_product'}]
+            
+        })
         .then(products => {
             return res.render('index', {
                 title: "Bienvenidos",

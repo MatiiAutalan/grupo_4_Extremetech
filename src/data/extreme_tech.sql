546accc05e961e783a3456277c284e1f9b28ec91
Joniruiz <ruiz.jonathan2018@gmail.com>
2021-09-30 14:05:42 -0300

    Sql y foreachs en la vista detail

--- src/data/extreme_tech.sql
+++ src/data/extreme_tech.sql
@@ -26,7 +26,7 @@ CREATE TABLE `brands` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -35,7 +35,7 @@ CREATE TABLE `brands` (
 
 LOCK TABLES `brands` WRITE;
 /*!40000 ALTER TABLE `brands` DISABLE KEYS */;
-INSERT INTO `brands` VALUES (1,'Logitech'),(2,'Asus'),(3,'HyperX'),(4,'Kingston'),(5,'Lg'),(6,'Samsung'),(7,'Sony'),(8,'Nisuta'),(9,'Redragon'),(10,'Vsg'),(11,'Trust'),(12,'Lenovo'),(13,'Cooler Master'),(14,'Intel'),(15,'Amd'),(16,'Gigabyte'),(17,'Nvidia'),(18,'Edifier'),(19,'Corsair'),(20,'Hp'),(21,'Top House'),(22,'Xiaomi');
+INSERT INTO `brands` VALUES (1,'Logitech'),(2,'Asus'),(3,'HyperX'),(4,'Kingston'),(5,'Lg'),(6,'Samsung'),(7,'Sony'),(8,'Nisuta'),(9,'Redragon'),(10,'Vsg'),(11,'Trust'),(12,'Lenovo'),(13,'Cooler Master'),(14,'Intel'),(15,'Amd'),(16,'Gigabyte'),(17,'Nvidia'),(18,'Edifier'),(19,'Corsair'),(20,'Hp'),(21,'Top House'),(22,'Xiaomi'),(27,'ExtremeTech');
 /*!40000 ALTER TABLE `brands` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -50,7 +50,7 @@ CREATE TABLE `categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -59,7 +59,7 @@ CREATE TABLE `categories` (
 
 LOCK TABLES `categories` WRITE;
 /*!40000 ALTER TABLE `categories` DISABLE KEYS */;
-INSERT INTO `categories` VALUES (1,'Perifericos'),(2,'Alimentacion'),(3,'Almacenamiento'),(4,'Microprocesadores'),(5,'Motherboards'),(6,'Memorias'),(7,'Placas de Video'),(8,'Refrigeracion'),(9,'Gabinetes'),(11,'Audio'),(12,'Software'),(13,'Monitores');
+INSERT INTO `categories` VALUES (1,'Perifericos'),(2,'Alimentacion'),(3,'Almacenamiento'),(4,'Microprocesadores'),(5,'Motherboards'),(6,'Memorias'),(7,'Placas de Video'),(8,'Refrigeracion'),(9,'Gabinetes'),(11,'Audio'),(12,'Software'),(13,'Monitores'),(14,'Notebooks'),(15,'pcArmadas');
 /*!40000 ALTER TABLE `categories` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -77,7 +77,7 @@ CREATE TABLE `images_product` (
   PRIMARY KEY (`id`),
   KEY `FK_75d0a37d-7e7a-48fb-accd-7f917f43e2b0` (`product_id`),
   CONSTRAINT `FK_75d0a37d-7e7a-48fb-accd-7f917f43e2b0` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -86,7 +86,7 @@ CREATE TABLE `images_product` (
 
 LOCK TABLES `images_product` WRITE;
 /*!40000 ALTER TABLE `images_product` DISABLE KEYS */;
-INSERT INTO `images_product` VALUES (15,'1632840731436.png',13),(16,'1632840731438.png',13),(17,'1632840731447.jpg',13),(18,'1632842305759.png',14),(19,'1632842305761.png',14),(20,'1632842305772.jpg',14),(21,'1632846064423.png',15),(22,'1632846064425.png',15),(23,'1632846064435.jpg',15);
+INSERT INTO `images_product` VALUES (15,'1632840731436.png',13),(16,'1632840731438.png',13),(17,'1632840731447.jpg',13),(18,'1632842305759.png',14),(19,'1632842305761.png',20),(20,'1632842305772.jpg',14),(21,'1632846064423.png',15),(22,'1632846064425.png',17),(23,'1632846064435.jpg',19),(24,'default.png',16);
 /*!40000 ALTER TABLE `images_product` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -100,7 +100,7 @@ DROP TABLE IF EXISTS `products`;
 CREATE TABLE `products` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `price` int(11) NOT NULL,
-  `description` varchar(255) NOT NULL,
+  `description` text NOT NULL,
   `discount` int(11) DEFAULT NULL,
   `category_id` int(11) NOT NULL,
   `brand_id` int(11) NOT NULL,
@@ -110,7 +110,7 @@ CREATE TABLE `products` (
   KEY `FK_fa2bf439-6a52-48a3-8374-533f3b54f478` (`brand_id`),
   CONSTRAINT `FK_b0cd8467-b7ef-4002-9744-59555271368a` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`),
   CONSTRAINT `FK_fa2bf439-6a52-48a3-8374-533f3b54f478` FOREIGN KEY (`brand_id`) REFERENCES `brands` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -119,7 +119,7 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (13,100,'asdasdsa',50,3,3,'pepe'),(14,100,'qaaaaa',50,1,2,'Jona'),(15,100,'aaaaaa',50,3,3,'Cafetera del niño kevin');
+INSERT INTO `products` VALUES (13,100,'asdasdsa',50,3,3,'pepe'),(14,100,'qaaaaa',50,1,2,'Jona'),(15,100,'aaaaaa',50,3,3,'Cafetera del niño kevin'),(16,100,'Notebook super wow',10,14,2,'Hp'),(17,43000,'PC Ideal para juegos, oficina y mucho más!\r\nPodes jugar juegos como Fortnite, Roblox, Minecraft, GTA, CsGo, Pubg, FIFA, PES y muchísimos más!\r\n\r\n\r\nESPECIFICACIONES\r\n\r\nMicroprocesador AMD Athlon 3000G Socket AM4 Con Vega 3 integrada\r\n\r\nMother A320 Asus / Gigabyte /MSI\r\n\r\nDisco 240Gb Ssd\r\n\r\nMemoria 8 Gb Ddr4\r\n\r\nGabinete Kit con Fuente de 500W\r\n\r\nTeclado , Mouse y parlantes de Regalo\r\n\r\nSistema Operativo Windows 10 Trial\r\n\r\nSi deseas realizar alguna modificación, consultanos y con gusto te asesoramos!',10,15,27,'Pc Gamer basica'),(19,208000,'PC Gamer gama media\r\nPodes jugar juegos todos los juegos del mercado en una excelente calidad! \r\n\r\n\r\nESPECIFICACIONES\r\n\r\nMicroprocesador  I5 10400F\r\n\r\nGPU Nvidia GeForce RTX 3060\r\n\r\nMother H410 Asus / Gigabyte /MSI\r\n\r\nDisco 240Gb Ssd\r\n\r\nDisco 1tb hdd\r\n\r\nMemoria 16 Gb Ddr4\r\n\r\nGabinete gamer y fuente de 650w\r\n\r\nSistema Operativo Windows 10 \r\n\r\nSi deseas realizar alguna modificación, consultanos y con gusto te asesoramos!',10,15,27,'Pc Gamer media'),(20,1000000,'PC Gamer gama ULTRA\r\n\r\nJuega todos los juegos del mercado a su maxima calidad , 4k a altos FPS\r\n\r\nESPECIFICACIONES\r\n\r\nMicroprocesador AMD Ryzen 9 5950X 4.9GHz Turbo AM4 \r\n\r\n Placa de Video Gigabyte GeForce RTX 3090 24GB GDDR6X VISION OC\r\n\r\n Mother ASUS ROG X570 CROSSHAIR VIII FORMULA AM4\r\n\r\nDisco Solido SSD M.2 WD 2TB Black SN850 7000MB/s NVMe PCIe Gen4\r\n\r\nMemoria OLOy DDR4 64GB (2x32GB) 3000MHz Warhawk Black RGB CL16\r\n\r\nGabinete ASUS TUF GT501 Vidrio Templado White 3x120mm ARGB\r\n\r\nFuente ASUS ROG STRIX 750G 80 Plus Gold 750W Full Modular\r\n\r\nSi deseas realizar alguna modificación, consultanos y con gusto te asesoramos!',5,15,27,'Pc Gamer ULTRA');
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -154,7 +154,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Jona','R','1155887788','asd@gmail.com','asd123',0,'user.jpg',333333,'Avenida siempre viva 123',1602,'buenos aires'),(2,'session muere1','as12312312dsadasd','12313','mail@mail.com','$2b$10$Qrkz7ks/gA3c5/GTmKkF8egrcOsAFAtEkiBjvQ3MTSzrUMXLqt3Vm',1,'1632714650153.png',123,'Asd direccionasa',1234,'Buenos Aires'),(5,'Gonza','Cometortitas','1144887755','mail1@mail.com','$2b$10$5tkjSYIJzKR/7dPL6GU09u0o/zF7p0exWrBRrqU8dlsLluleblp2e',0,'user.jpg',NULL,NULL,NULL,NULL),(43,'juan','topo','1127880088','kevin@mail.com','$2b$10$XIX16AnwsComerbpMu7B/e.TbWxmtHiUuRAKnYSkUekxDtEfjlGOq',0,'user.jpg',NULL,NULL,NULL,NULL),(44,' Gonza  ','pelozo','1155228877','kevin123@mail.com','$2b$10$WSpUjYKHmVav6Gc6ypBdEeUkOT2Q2ePK/OQr.zKiPJgOV..9hkuPW',0,'user.jpg',NULL,NULL,NULL,NULL),(46,' aaaa','asd','1127880088','proband11o@asd.com','$2b$10$SlUmQazAHSJrSbOMK7Q50OiWb7Qep8T/U88619Z1wdTFUZwV38duK',0,'user.jpg',NULL,NULL,NULL,NULL),(48,'asdasd','racedo','1155228877','keviniii@gmail.com','$2b$10$WmNlNEUwrSzMSs4/Kt45EOQJXJw265n.12GA91IHvbPNdop4PBuD2',0,'user.jpg',NULL,NULL,NULL,NULL),(49,'kevin','almada','1127885555','kevin10@gmail.com','$2b$10$czJd78qfNCstHzoYRU.SMOjIOCEkhU3St4SYhK7YRkIYJUnyGKps2',0,'user.jpg',NULL,NULL,NULL,NULL),(50,'asd','asd','1127885555','pruebanoadmin@gmail.com','$2b$10$sYyD0cWkliIxqFueaFYczewoEJLLDjqB0dqUbpbrXZhGMpTGFpU.S',1,'user.jpg',NULL,NULL,NULL,NULL);
+INSERT INTO `users` VALUES (1,'Jona','R','1155887788','asd@gmail.com','asd123',1,'user.jpg',333333,'Avenida siempre viva 123',1602,'buenos aires'),(2,'session muere1','as12312312dsadasd','12313','mail@mail.com','$2b$10$Qrkz7ks/gA3c5/GTmKkF8egrcOsAFAtEkiBjvQ3MTSzrUMXLqt3Vm',1,'1632714650153.png',123,'Asd direccionasa',1234,'Buenos Aires'),(5,'Gonza','Cometortitas','1144887755','mail1@mail.com','$2b$10$5tkjSYIJzKR/7dPL6GU09u0o/zF7p0exWrBRrqU8dlsLluleblp2e',0,'user.jpg',NULL,NULL,NULL,NULL),(43,'juan','topo','1127880088','kevin@mail.com','$2b$10$XIX16AnwsComerbpMu7B/e.TbWxmtHiUuRAKnYSkUekxDtEfjlGOq',0,'user.jpg',NULL,NULL,NULL,NULL),(44,' Gonza  ','pelozo','1155228877','kevin123@mail.com','$2b$10$WSpUjYKHmVav6Gc6ypBdEeUkOT2Q2ePK/OQr.zKiPJgOV..9hkuPW',0,'user.jpg',NULL,NULL,NULL,NULL),(46,' aaaa','asd','1127880088','proband11o@asd.com','$2b$10$SlUmQazAHSJrSbOMK7Q50OiWb7Qep8T/U88619Z1wdTFUZwV38duK',0,'user.jpg',NULL,NULL,NULL,NULL),(48,'asdasd','racedo','1155228877','keviniii@gmail.com','$2b$10$WmNlNEUwrSzMSs4/Kt45EOQJXJw265n.12GA91IHvbPNdop4PBuD2',0,'user.jpg',NULL,NULL,NULL,NULL),(49,'kevin','almada','1127885555','kevin10@gmail.com','$2b$10$czJd78qfNCstHzoYRU.SMOjIOCEkhU3St4SYhK7YRkIYJUnyGKps2',0,'user.jpg',NULL,NULL,NULL,NULL),(50,'asd','asd','1127885555','pruebanoadmin@gmail.com','$2b$10$sYyD0cWkliIxqFueaFYczewoEJLLDjqB0dqUbpbrXZhGMpTGFpU.S',1,'user.jpg',NULL,NULL,NULL,NULL);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -171,4 +171,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-09-28 23:49:44
+-- Dump completed on 2021-09-30 14:03:11

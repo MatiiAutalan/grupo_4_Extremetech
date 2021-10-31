6642aa687e9c045597c206a4eaaa03fef7d6a6cf
Joniruiz <ruiz.jonathan2018@gmail.com>
2021-10-30 23:37:10 -0300

    Formulario fixeado el de register

--- public/javascript/validationRegisterFront.js
+++ public/javascript/validationRegisterFront.js
@@ -21,7 +21,7 @@ regExEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i,
 regExPass = /^(?=.*\d)(?=.*[a-z]).{6,12}$/;
 let iconError = '<i class="fas fa-times-circle"></i>'
 let iconCheck = '<i class="fas fa-check"></i>'
-
+let validationsErrors = false
 
 
 
@@ -32,15 +32,18 @@ window.addEventListener('load',()=>{
             case !$email.value.trim():
                 erroresEmail.innerHTML =`${iconError} El email es obligatorio `
                 $email.style.border = "3px solid red"
+                validationsErrors = true
                 break;
                 case !regExEmail.test($email.value):
                     erroresEmail.innerHTML = `${iconError} Debe ingresar un email valido `
                     $email.style.border = "3px solid red"
+                    validationsErrors = true
                     break;
                 default:
                     erroresEmail.innerHTML= ""
                     $email.classList.remove('class')
                     $email.style.border = "3px solid green"
+                    validationsErrors = false
                     //erroresEmail.style.color = 'green'
                     
                  
@@ -51,17 +54,20 @@ window.addEventListener('load',()=>{
       switch (true) {
           case !$password.value.trim():
             $passwordErrors1.innerHTML= 'Contraseña obligatoria'
-            $password.style.border = "3px solid red"
+            $password.style.border = "3px solid red".
+            validationsErrors = true
               break;
               case !regExPass.test($password.value):
                  $passwordErrors1.innerHTML ='La contraseña debe tener: entre 6 o 12 caracteres, al menos una mayúscula, una minúscula y un número';
             $password.style.border = "3px solid red"
+            validationsErrors = true
             break
       
           default:
            $passwordErrors1.innerHTML= ""    
             $password.classList.remove('class')
             $password.style.border = "3px solid green"
+            validationsErrors = false
 
               break;
       }
@@ -73,15 +79,18 @@ window.addEventListener('load',()=>{
           case !$password2.value.trim():
               $passError2.innerHTML = `${iconError} Debe repetir su contraseña`
               $password2.style.border = "3px solid red"
+              validationsErrors = true
               break;
               case $password2.value !== $password.value:
                 
                 passError2.innerHTML = `${iconError} Las contraseñas no coinciden`
                 $password2.style.border = "3px solid red"
+                validationsErrors = true
                 break;
           default:
               passError2.innerHTML = ""
               $password2.style.border = "3px solid green"
+              validationsErrors = false
 
               break;
       }
@@ -93,13 +102,16 @@ window.addEventListener('load',()=>{
     switch (true) {
         case $nombre.value.trim().length <= 2 :
             $nombreError.innerHTML = "El nombre debe ser mayor a 2 caracteres"
+            validationsErrors = true
             break;
         case !regExAlpha.test($nombre.value) :
             $nombreError.innerHTML = "Debes ingresar un nombre valido"
+            validationsErrors = true
               break;
         default:
             $nombreError.innerHTML= ""
             $nombre.style.border = "3px solid green"
+            validationsErrors = false
             break;
     } 
 })
@@ -107,13 +119,16 @@ window.addEventListener('load',()=>{
     switch (true) {
         case $apellido.value.trim().length <= 2 :
             $apellidoError.innerHTML = "El apellido debe ser mayor a 2 caracteres"
+            validationsErrors = true
             break;
         case !regExAlpha.test($apellido.value) :
               $apellidoError.innerHTML = "Debes ingresar un apellido valido"
+              validationsErrors = true
               break;
         default:
           $apellidoError.innerHTML= ""
           $apellido.style.border = "3px solid green"
+          validationsErrors = false
             break;
     } 
 }) 
@@ -121,11 +136,13 @@ $telefono.addEventListener('change',function(){
     switch (true) {
         case $telefono.value.trim().length <= 10 :
             telefonoError.innerHTML = "Telefono invalido"
+            validationsErrors = true
             break;
             default:
           $telefono.value.trim()
           telefonoError.innerHTML= ""
           telefonoError.style.backgroundColor = '#70ff8b'
+          validationsErrors = false
             break;
     } 
 
@@ -146,9 +163,9 @@ $form.addEventListener('submit',function(event){
       } 
   }
 
-    if(error == 0){
+    if(error == false && !validationsErrors){
         console.log('Formulario Enviado');
-        $form.submit()
+        //$form.submit()
     }
 
   })

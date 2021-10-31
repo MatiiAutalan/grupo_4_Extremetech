c4b22d97da3b2ac97ff9aac4f29ab69e07d171f7
Joniruiz <ruiz.jonathan2018@gmail.com>
2021-10-30 23:59:40 -0300

    Validador del registro backend Fixed

--- src/validations/validationRegister.js
+++ src/validations/validationRegister.js
@@ -17,7 +17,9 @@ module.exports = [
       })
     }),
     check('nombre').notEmpty().withMessage('Campo obligatorio'),
+    check('nombre').isLength({min:2, max:10}),
     check('password').notEmpty().withMessage('Campo obligatorio'),
+    check('password').isLength({min:6, max:12}),
     //check('apellido').notEmpty().withMessage('Campo obligatorio'),
     //check('telefono').notEmpty().withMessage('Campo obligatorio')
     //.isLength({ min:8 , max: 12}).withMessage('maximo 10 caracteres'),
